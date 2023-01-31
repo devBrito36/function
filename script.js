5f8a6c3f290d14a9e6a455e2dbc112164c4bae90
felpiz230 <99624132+felpiz230@users.noreply.github.com>
2023-01-30 23:45:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function mensagem(){
+
+    console.log("segunda atividade do modulo dois")
+}
+mensagem()
+
+function Nome(nome) {
+    console.log(`Meu nome é ${nome}`);
+}
+Nome("Filipe")
+
+function Ramon(nome,idade,musica) {
+    console.log(`Meu nome é ${nome}, tenho ${idade} anos de idade e meu estilo de música preferido é ${musica}`)
+}
+Ramon("Ramon","19","eletrônica")
+
+function lazer(filme,musica) {
+    console.log(`o filme ${filme} e a musica ${musica}`)
+}
+lazer("Frozen, uma aventura congelante","to say goodbye")
+
+function multi(X)
+    
